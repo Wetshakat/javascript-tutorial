065d2aa5d54fc7182e5b810367237ee7693a6231
wetshakat <pc@racy.lan>
2024-10-07 15:49:41 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let bam = "dyelshak ishaku wetshakat"
+let age = 35
+let nationality ="nigeria"
+
+// console.log(bam + " is " + nationality + " is " + age);
+
+//console.log(bam.toLocaleUpperCase());
+
+//  console.log(bam.charCodeAt(1));
+// console.log(bam.charAt(11));
+  
+  console.log(bam.slice(8,15));
+console.log(bam.substring(8,25));
